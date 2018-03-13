9b717a17c3167855aa81180787ef6cdf6714a598
Todd Halfpenny <todd@mobilecaddy.net>
2018-03-13 13:18:31 +0000

    first commit

---
+++ src/pages/init/init.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import  { startup, getStatus } from 'mobilecaddy-utils/startup';
+// import  { startup, getStatus } from '../../assets/js/startup';
+import { TabsPage } from '../tabs/tabs';
+
+
+@Component({
+  selector: 'page-init',
+  templateUrl: 'init.html'
+})
+
+export class InitPage {
+
+  initStatus: '';
+  statusPoll: () => void;
+  pollId;
+
+  constructor(public navCtrl: NavController, public platform: Platform) {
+
+    const logModule = 'InitPage';
+
+    if ( location.hostname == 'localhost' || ! navigator.appVersion.includes('obile') ) {
+      console.log('Running in CodeFlow');
+
+      this.pollId = window.setTimeout(this.statusPoll = () => {
+        this.initStatus = getStatus();
+        console.log('getStatus', this.initStatus);
+        this.pollId = setTimeout(this.statusPoll, 1000);
+      }, 1000);
+
+      // Running in CodeFlow
+      startup().then(res =>  {
+        console.log("res", res);
+        window.clearTimeout( this.pollId );
+        this.navCtrl.setRoot(TabsPage, {}, {animate: true, direction: 'forward'});
+      }).catch(e => {
+        window.clearTimeout( this.pollId );
+        console.error(logModule, e);
+      });
+
+    } else {
+      // On a device, wait for platform.ready()
+      this.pollId = window.setTimeout(this.statusPoll = () => {
+        this.initStatus = getStatus();
+        console.log('getStatus', this.initStatus);
+        this.pollId = setTimeout(this.statusPoll, 100);
+      }, 10);
+
+    	this.platform.ready().then((readySource) => {
+        console.log('Platform ready from', readySource);
+        return startup();
+       }).then(res =>  {
+        window.clearTimeout( this.pollId );
+        console.log("res", res);
+        this.navCtrl.setRoot(TabsPage, {}, {animate: true, direction: 'forward'});
+      }).catch(e => {
+        window.clearTimeout( this.pollId );
+        console.error(logModule, e);
+      })
+    }
+
+  }
+
+}
