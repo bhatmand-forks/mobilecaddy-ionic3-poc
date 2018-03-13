574a469081d2f082c439993449d5c18a043b6541
Todd Halfpenny <todd@mobilecaddy.net>
2018-03-13 13:19:09 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,24 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { InitPage } from '../pages/init/init';
+
+// DEV STUFF
+import {isDevMode} from "@angular/core";
+
+import * as devUtils from 'mobilecaddy-utils/devUtils';
+import * as _ from 'underscore';
+
+if (isDevMode()) {
+  window['devUtils'] = devUtils;
+  window['_'] = _;
+}
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = InitPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
